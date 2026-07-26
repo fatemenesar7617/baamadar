a4f03ac186276069289d92495131b95794e77bac
fatiNesarvand <fatinesar5@gmail.com>
2026-07-26 23:35:00 +0330

    update readme

--- components/Header/Header.jsx
+++ components/Header/Header.jsx
@@ -1,4 +1,5 @@
 import { useRouter } from "next/router";
+import Link from "next/link";
 
 export default function Header({ onCartClick, cartCount = 0 }) {
   const router = useRouter();
@@ -11,6 +12,12 @@ export default function Header({ onCartClick, cartCount = 0 }) {
         <img src="/logos/direction-left 01.svg" alt="direction" className="w-5 h-5"/>
         <img src="/logos/madarlogo.svg" alt="logo" className="w-24 h-auto"/>
       </div>
+      <div className="flex items-center gap-4">
+      <Link href="/login">
+  <button className="text-xs border border-gray-500 text-[#E86B42] rounded-xl px-2 py-1">
+       ورود به سایت
+  </button>
+</Link>
       <div
         onClick={onCartClick}
         className="relative cursor-pointer"
@@ -20,7 +27,9 @@ export default function Header({ onCartClick, cartCount = 0 }) {
           <span className="absolute -top-1 -left-1 w-5 h-5 bg-[#E86B42] text-white text-xs rounded-full flex items-center justify-center font-peyda font-bold">
             {cartCount}
           </span>
+          
         )}
+        </div>
       </div>
     </header>
   );
